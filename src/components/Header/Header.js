fd1651ff25ca8ac8f44e4733f0ff6582762fef5a
ShowkatSumon1 <showkatsumon10@gmail.com>
2022-10-13 20:14:53 +0600

    file update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="logo" className="logo" />
+            <nav className="nav">
+                <a href="/shop" className="link">Shop</a>
+                <a href="/review" className="link">Order Review</a>
+                <a href="manage" className="link">Manage Inventory here</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Shop.css';
+import fakeData from '../../fakeData';
+
+const Shop = () => {
+    console.log(fakeData);
+    return (
+        <div>
+            <h2>Shoping</h2>
+        </div>
+    );
+};
+
+export default Shop;
